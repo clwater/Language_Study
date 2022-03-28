4cfa6e2d776bb0b724e81b8abdcf3d32b88bb391
clwater <gzb@clwater.top>
2022-03-28 16:33:22 +0800

    Wip(server): init

---
+++ src/main/kotlin/com/clwater/language_study/server/LanguageStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.clwater.language_study.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LanguageStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<LanguageStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/clwater/language_study/server/LanguageStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clwater.language_study.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LanguageStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
